430ee898249abf2f1726dab7cbd88436c63ba48d
Ahmed Harabi <123783548+ahmedharabi@users.noreply.github.com>
2025-10-07 12:14:54 +0100

    Time: 11 ms (7.65%), Space: 43.5 MB (73.1%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> s=new Stack<>();
+        String[]parts=path.split("/");
+        
+        
+        for(int i=0;i<parts.length;i++){
+            if(parts[i].equals("..")&&!s.empty()){
+                System.out.println("waaaaaa");
+                s.pop();
+            }else if(!parts[i].equals(" ") && !parts[i].equals("") && !parts[i].equals(".") && !parts[i].equals("..")){
+                s.push(parts[i]);
+            }
+        }
+        String ans=String.join("/", s);
+        return "/"+ans;
+    }
+}
