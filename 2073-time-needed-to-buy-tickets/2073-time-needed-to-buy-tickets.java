54284edbaab0b65d59f7c3f66f5db9da90494c7f
Ahmed Harabi <123783548+ahmedharabi@users.noreply.github.com>
2025-10-19 12:17:32 +0100

    Time: 2 ms (50.58%), Space: 41.4 MB (63.56%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+       int ans=0;
+       while(tickets[k]!=0){
+
+            for(int i=0;i<tickets.length;i++){
+            if(tickets[i]!=0){
+                tickets[i]-=1;
+                ans++;
+            }
+            if(tickets[k]==0)return ans;
+       }
+
+
+
+       }
+       
+       return ans; 
+    }
+}
